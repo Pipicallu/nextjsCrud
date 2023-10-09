4dbc594575b5b1c00437bc6bee317f547acc94da
Tom Vassallo <thomas.vassallo@gmail.com>
2023-10-09 14:40:02 +0100

    just some playing around with queries

--- src/app/Mongo/index.js
+++ src/app/Mongo/index.js
@@ -2,16 +2,10 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri = process.env.SECRET_URI;
-console.log(uri);
+console.log(uri, "is you forget?");
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+const client = new MongoClient(uri);
 
 async function run() {
   try {
@@ -21,8 +15,9 @@ async function run() {
   
     const query = {note: 'Wash yo ass.'};
     const note = await notes.findOne(query);
-    const allNotes =  notes.find();
-    console.log(notes);
+    const allNotes = await notes.find().toArray();
+    console.log(note);
+    console.log(allNotes);
     
   } finally {
     // Ensures that the client will close when you finish/error
