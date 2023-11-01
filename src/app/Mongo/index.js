2c6e5bc6af91e4f170583576b11bfd5be0dd455e
Tom Vassallo <thomas.vassallo@gmail.com>
2023-11-01 22:22:20 +0000

    displaying the notes

--- src/app/Mongo/index.js
+++ src/app/Mongo/index.js
@@ -1,8 +1,7 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = process.env.SECRET_URI;
-console.log(uri, "is you forget?");
+const uri = "https://swapi.dev/api/people/1"
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,51 @@ import React from 'react'
 import {Note} from './note'
 import {WriteNote} from './NewNote'
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = process.env.SECRET_URI;
 
 
-export default function Page(): React.JSX.Element {
+async function getNotes() {
+      const client = new MongoClient(uri);
+      const database = client.db('basicNotesApp');
+      const notes = database.collection('Notes');
+      const allNotes = await notes.find().toArray();
+      return allNotes 
+  }
+
+
+async function getData() {
+    const res = await fetch('https://swapi.dev/api/people/1')
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+   console.log("******", res);
+    if (!res.ok) {
+      // This will activate the closest error.js Error Boundary
+      throw new Error('Failed to fetch data')
+    }
+   
+    return res.json()
+  }
+
+
+ const MyNotes = ({data}) => {
     return (
         <>
          <div><h2>Write your notes below</h2></div>
-         <Note note={'hello'} />
-        <ul>{notes}</ul>
-         <WriteNote />
+         <ul>
+            {data.map(n => <li><Note note={n.note} /></li>)}
+         </ul>
         </>
-
 
     )
 }
+
+export default async function Page() {
+    const data = await getNotes()
+    console.log(data)
+    return <MyNotes data={data} />
+  }
  
 
 
